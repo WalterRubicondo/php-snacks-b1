7ccf6709619d55825b24bc4c0ed71292d47d2e70
Walter <walterrubicondo@gmail.com>
2021-04-29 17:19:55 +0200

    Fine Esercizio

--- phpsnack3/index.php
+++ phpsnack3/index.php
@@ -9,4 +9,51 @@ https://www.codepile.net/pile/R2K5d68z -->
 
 <?php
 
- ?>
+  $posts = [
+      '10/01/2019' => [
+          [
+              'title' => 'Post 1',
+              'author' => 'Michele Papagni',
+              'text' => 'Testo post 1'
+          ],
+          [
+              'title' => 'Post 2',
+              'author' => 'Michele Papagni',
+              'text' => 'Testo post 2'
+          ],
+      ],
+      '10/02/2019' => [
+          [
+              'title' => 'Post 3',
+              'author' => 'Michele Papagni',
+              'text' => 'Testo post 3'
+          ]
+      ],
+      '15/05/2019' => [
+          [
+              'title' => 'Post 4',
+              'author' => 'Michele Papagni',
+              'text' => 'Testo post 4'
+          ],
+          [
+              'title' => 'Post 5',
+              'author' => 'Michele Papagni',
+              'text' => 'Testo post 5'
+          ],
+          [
+              'title' => 'Post 6',
+              'author' => 'Michele Papagni',
+              'text' => 'Testo post 6'
+          ]
+      ],
+  ];
+
+  foreach ($posts as $data => $contenuto) { ?>
+    <p> <?= $data ?> </p>
+    <?php
+      for ($i=0; $i < count($contenuto) ; $i++) { ?>
+        <p><?= $contenuto[$i]["title"] ?></p>
+        <p><?= $contenuto[$i]["author"] ?></p>
+        <p><?= $contenuto[$i]["text"] ?></p>
+      <?php  } ?>
+  <?php  } ?>
